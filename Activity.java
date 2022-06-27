9dab2590dc73822d2a0e39219c08a89214de2505
Muskan Munir Mujawar <106859307+Alsaba123@users.noreply.github.com>
2022-06-27 17:57:40 +0530

    Add files via upload

---
+++ Activity.java
@@ -0,0 +1,12 @@
+package p1;
+//import java.util.*;
+class Activity{
+	String string1;
+	String string2;
+	String operator;
+	public Activity(String string1, String string2, String operator) {
+		this.string1 = string1;
+		this.string2 = string2;
+		this.operator = operator;
+	}
+}
