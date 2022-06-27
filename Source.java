86e1a9936aa89851c2db2d55af8081d218e2bf41
Muskan Munir Mujawar <106859307+Alsaba123@users.noreply.github.com>
2022-06-27 14:00:39 +0530

    Add files via upload

---
+++ Source.java
@@ -0,0 +1,21 @@
+package p1;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+public class Source {
+
+	
+		public static void main(String args[] ) throws Exception {
+			
+			Mobile obj = new Mobile();
+			System.out.println(obj.addMobile("Oppo", "K3"));
+			System.out.println(obj.getModels("Oppo"));
+			System.out.println(obj.buyMobile("Oppo", "K3"));
+			}
+			}
+
+	
+
+
